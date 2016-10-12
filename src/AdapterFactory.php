afb162ca65e8d3fdb219d96454f3a75aa9be4777
Vladislav Bezverkhiy <bezverhiy@a2design.biz>
2016-10-12 17:32:32 +0600

    Updated AdpaterFactory::getAdapter method

--- src/AdapterFactory.php
+++ src/AdapterFactory.php
@@ -3,15 +3,55 @@
 namespace A2Design\AIML;
 
 use A2Design\AIML\Adapters\FileAdapter;
+use A2Design\AIML\Error\Dictionary\NotFoundException;
+use A2Design\AIML\Error\AdapterNotFoundException;
+use A2Design\AIML\Contracts\Adapter;
 
 class AdapterFactory {
 
-    public function getAdapter($aiml)
+
+
+    static protected $providers = [
+        'file' => '\A2Design\AIML\Adapters\FileAdapter',
+    ];
+
+    /**
+     * Returns adapter object by provided string
+     *
+     * @param  string $aiml dictionary string
+     *
+     * @throws NotFoundException
+     *
+     * @return Adapter
+     */
+    public function getAdapter($aiml, $type = 'file')
     {
-        if (file_exists($aiml)) {
-            $adapter = new FileAdapter;
-            $adapter->setAIML($aiml);
-            return $adapter;
+        $adapter = $this->getByType($type);
+
+        if (!$adapter->checkExists($aiml)) {
+            throw new NotFoundException;
         }
+
+        $adapter->setAIML($aiml);
+        return $adapter;
+    }
+
+
+    /**
+     * Returns new class instance by provided type
+     *
+     * @param  string $type internal adapter type
+     *
+     * @throws AdapterNotFoundException
+     *
+     * @return Adapter
+     */
+    protected function getByType($type)
+    {
+        if (!isset(self::$providers[$type])) {
+            throw new AdapterNotFoundException;
+        }
+
+        return new self::$providers[$type];
     }
 }
