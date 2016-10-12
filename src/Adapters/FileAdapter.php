ad19cdd19b1a60b162f8dc01540a1c4fc0dccc5b
Vladislav Bezverkhiy <bezverhiy@a2design.biz>
2016-10-12 17:32:16 +0600

    Updated FileAdapter

--- src/Adapters/FileAdapter.php
+++ src/Adapters/FileAdapter.php
@@ -14,22 +14,62 @@ class FileAdapter extends Adapter {
         $this->aimlParser = new AIMLParser();
     }
 
+    /**
+     * Checks AIML content existance
+     *
+     * @param  string $aiml AIML content
+     *
+     * @return boolean
+     */
+    public function checkExists($aiml)
+    {
+        return file_exists($aiml);
+    }
+
+
+    /**
+     * Saves AIML content to cache property
+     *
+     * @param string $aimlContent AIML Content
+     */
     public function setAIML($aimlContent)
     {
         $content = file_get_contents($aimlContent);
         $this->content = new SimpleXMLElement($content);
     }
 
+
+    /**
+     * Returns AIML content
+     *
+     * @return SimpleXMLElement
+     */
     public function getAIML()
     {
         return $this->content;
     }
 
+
+    /**
+     * Prepare AIML content for parser
+     *
+     * @param  SimpleXMLElement $content AIML XML object
+     *
+     * @return SimpleXMLElement
+     */
     public function prepareAIML($content)
     {
         return $content;
     }
 
+
+    /**
+     * Search pattern in AIML
+     *
+     * @param  string $pattern AIML question pattern
+     *
+     * @return Answer
+     */
     protected function _searchPattern($pattern)
     {
         $this->parseAIML($this->content, $pattern);
@@ -37,12 +77,27 @@ class FileAdapter extends Adapter {
         return $rawAnswer;
     }
 
+
+    /**
+     * Seatch answer by provided question
+     *
+     * @param  string $question AIML question pattern
+     *
+     * @return mixed
+     */
     protected function _searchAnswer($question)
     {
         $categories = $this->searchCategory($question);
         return $categories;
     }
 
+
+    /**
+     * Search category by question
+     *
+     * @param  string $question AIML question pattern
+     * @return null
+     */
     protected function _searchCategory($question)
     {
         $aiml = $this->parseAIML($this->content, $question);
